803186adc955db16e4e592834fdb410f88a8e740
usernme <emailName>
2018-04-12 19:24:32 +0800

    td-idf

--- TF-IDF/tf_idf.py
+++ TF-IDF/tf_idf.py
@@ -7,21 +7,34 @@ Created on Thu Apr 12 18:11:30 2018
  
 import word_list
 
-from os import path
+import os
 from sklearn import feature_extraction  
 from sklearn.feature_extraction.text import TfidfTransformer  
 from sklearn.feature_extraction.text import CountVectorizer
 
-current_path = path.dirname(__file__)
-parent_path =  path.dirname(current_path) #返回当前文件父级目录  
-text_path = '/testSpider/output/output_1.txt' #设置要分析的文本路径
+def get_file_name(file_dir):   
+    L=[]   
+    for root, dirs, files in os.walk(file_dir):  
+        for file in files:  
+            if os.path.splitext(file)[1] == '.txt':  
+                L.append(os.path.splitext(file)[0])  
+    return L  
+
+
+current_path = os.path.dirname(__file__)
+parent_path =  os.path.dirname(current_path) #返回当前文件父级目录  
+text_path = '/testSpider/output/' #设置要分析的文本路径
 filePath = parent_path + text_path
 
 total_text = []
-
 wordSeg = word_list.WordSeg()
-text = wordSeg.segment(filePath)
-total_text.append(text)
+
+file_list = get_file_name(filePath)
+for file_name in file_list:
+    text_path = '/testSpider/output/' + file_name + '.txt' #设置要分析的文本路径
+    filePath = parent_path + text_path
+    text = wordSeg.segment(filePath)
+    total_text.append(text)
 
 vectorizer=CountVectorizer()#该类会将文本中的词语转换为词频矩阵，矩阵元素a[i][j] 表示j词在i类文本下的词频  
 transformer=TfidfTransformer()#该类会统计每个词语的tf-idf权值  
@@ -30,13 +43,12 @@ tfidf=transformer.fit_transform(vectorizer.fit_transform(total_text))
 word=vectorizer.get_feature_names()#获取词袋模型中的所有词语  
 weight=tfidf.toarray()#将tf-idf矩阵抽取出来，元素a[i][j]表示j词在i类文本中的tf-idf权重  
 for i in range(len(weight)):#打印每类文本的tf-idf词语权重，第一个for遍历所有文本，第二个for便利某一类文本下的词语权重  
-    f = open(current_path + '/tf_idf_result/output_' + str(i) +'_tfidf.txt', 'w',\
+    f = open(current_path + '/tf_idf_result/' + file_list[i] +'_tfidf.txt', 'w',\
              encoding='UTF-8')
     for j in range(len(word)):  
         f.write(word[j] + ' ')
         f.write(str(weight[i][j]) + '\n')
 #        f.write('\n')
     f.close()
-
         
 
