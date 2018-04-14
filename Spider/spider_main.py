bcce9c74873b9f5fce6b0f41c1e3a50e31b252c5
usernme <emailName>
2018-04-14 19:23:29 +0800

    Revert "Revert "uodate""
    This reverts commit 2fd2fbad91e71182788c04c02adb5330ac278e1b.

--- Spider/spider_main.py
+++ Spider/spider_main.py
@@ -19,7 +19,7 @@ class SpiderMain(object):
 				new_urls, new_data = self.parser.parse(new_url, html_cont)
 				self.urls.add_new_urls(new_urls)
 				self.outputer.collect_data(new_data)
-				if(count == 10):
+				if(count == 100):
 					break
 
 				count = count + 1
---
+++ TF_IDF/word_list.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 11 17:11:15 2018
+
+@author: Herbert
+"""
+import jieba
+
+class WordSeg():
+    """word segmentation algorithm"""
+    def __init__(self):
+        super(WordSeg, self).__init__()
+        
+    def segment(self, filePath):
+        print("word segmentation:")
+        original_text = open(filePath, 'r', encoding='UTF-8').read()
+        
+        seg_list = jieba.cut_for_search(original_text, HMM=False)  # 搜索引擎模式
+        text = " ".join(seg_list)
+        
+        print("word segmentation finish")
+        return text
